b6b61c72b26d4753ab808398508b534213c7a341
emil <rustediron@outlook.com>
2025-04-24 08:36:43 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -3,19 +3,35 @@
 // Date:
 
 // Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Constants
+let make = "Lexus";
+let model = "RX 450H";
+let color = "White";
+let year = 2013;
+let age = 2025-year;
+let ownIt = false;
 
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  document.getElementById('output').innerHTML = `<br>
+    Make: ${make}<br>
+    Model: ${model}<br>
+    Color: ${color}<br>
+    Year: ${year}<br>
+    Age: ${age}<br>
+  `;
 }
+
+// on document ready, run function
+$(document).ready(function() {
+  // load html file into specified div
+  $('nav#links').load('../navlinks.html');
+  
+  console.log("Printed");
+});
+// html load with help from https://www.geeksforgeeks.org/how-to-load-external-html-file-using-jquery/
+
 
 // let's get this party started
 main();
